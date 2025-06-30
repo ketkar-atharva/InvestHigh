e67b4cb73144fff112a7dcc124e4bfd340df1157
ketkar-atharva <atharvaketkar13@gmail.com>
2025-06-30 20:15:18 +0530

    added time period input add total value

--- fd.js
+++ fd.js
@@ -2,11 +2,15 @@ let input=document.getElementById("inputfd");
 let list=document.getElementById("acclist");
 let inamount=document.getElementById("inputam");
 let interest=document.getElementById("inputin");
+let year=document.getElementById("inputyear");
 let listam=document.getElementById("amlist");
 let listin=document.getElementById("inlist");
 let listval=document.getElementById("valuelist");
+let listyear=document.getElementById("yearlist");
 let array=[];
+let int=[];
 let a=1;
+let y=1;
 let i=1;
 let n=1;
 function addacc(){
@@ -44,10 +48,9 @@ function addin(){
    }else if(i<=10){
       let li=document.createElement("li");
       li.innerHTML=interest.value;
+      int[i]=interest.value;
       listin.appendChild(li);
-      let l=document.createElement("li");
-      l.innerHTML=(1+(interest.value/100));
-      listval.appendChild(l);
+      
       interest.value="";
       i++;
 
@@ -55,3 +58,19 @@ function addin(){
       alert("Sorry only 10 values can be added");
    }
 }
+function addyear(){
+   if(year.value==""){
+         alert("Please enter Period ");
+   }else if(y<=10){
+       let li=document.createElement("li");
+      li.innerHTML=year.value;
+      listyear.appendChild(li);
+       let l=document.createElement("li");
+      l.innerHTML= array[y]*(1+(Number(int[y])/100))^year.value;
+      listval.appendChild(l);
+      year.value="";
+      y++;
+
+   }
+}
+
