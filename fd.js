119e4d954571efdcf1a098af4d96ce7754e5ef20
ketkar-atharva <atharvaketkar13@gmail.com>
2025-06-30 19:45:13 +0530

    added input for aount and interest

--- fd.js
+++ fd.js
@@ -1,5 +1,13 @@
 let input=document.getElementById("inputfd");
 let list=document.getElementById("acclist");
+let inamount=document.getElementById("inputam");
+let interest=document.getElementById("inputin");
+let listam=document.getElementById("amlist");
+let listin=document.getElementById("inlist");
+let listval=document.getElementById("valuelist");
+let array=[];
+let a=1;
+let i=1;
 let n=1;
 function addacc(){
    if(input.value==""){
@@ -12,5 +20,38 @@ function addacc(){
     n++;
    }else{
     alert("Sorry only 10 Account can be added");
+   }
+}
+
+function addam(){
+   if(inamount.value==""){
+      alert("Please enter amount");
+   }else if(a<=10){
+      let li=document.createElement("li");
+      li.innerHTML=inamount.value;
+      listam.appendChild(li);
+      array[a]=inamount.value;
+      inamount.value="";
+      a++;
+
+   }else{
+      alert("Sorry only 10 amounts can abe added");
+   }
+}
+function addin(){
+   if(interest.value==""){
+      alert("Please enter Interest rate");
+   }else if(i<=10){
+      let li=document.createElement("li");
+      li.innerHTML=interest.value;
+      listin.appendChild(li);
+      let l=document.createElement("li");
+      l.innerHTML=(1+(interest.value/100));
+      listval.appendChild(l);
+      interest.value="";
+      i++;
+
+   }else{
+      alert("Sorry only 10 values can be added");
    }
 }
